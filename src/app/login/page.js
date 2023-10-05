76283211c2e725361f0cb173a8c20266d8f0c778
Rohitturkar <rohittur2002@gmail.com>
2023-10-05 10:57:47 +0530

    Update login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,9 +1,17 @@
+"use client"
 import Link from "next/link"
+import {useRouter} from 'next/navigation'
+
 const Login=()=>{
+    const router=useRouter();
     return(
         <div>
         <h1 className="font-bold">Login page</h1>
         <Link href="/">Go to Home page</Link>
+        <br/>
+        <button onClick={()=>router.push("/login/loginstudent")}>Go to student login page</button>
+        <br/>
+        <button onClick={()=>router.push("/login/loginteacher")}>Go to Tecaher login page</button>
         </div>
     )
 }
