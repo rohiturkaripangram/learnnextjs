7a406cb8a798413ffa8d493c8e52c64ac2467fdf
Rohitturkar <rohittur2002@gmail.com>
2023-10-05 12:22:17 +0530

    Update layout
    Conditional Layout

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,22 +1,31 @@
-import Link from "next/link"
+'use client'
+import Link from "next/link";
+import {usePathname} from "next/navigation";
 
-export default function Layout({children}){
-return(
+
+export default function Layout({ children }) {
+    const pathName=usePathname();
+   
+  return (
     <div>
-        <ul className='list-none flex justify-center items-center gap-10 p-4 bg-slate-700 text-white'>
-            <li >
-                <Link href="/login">Login Main</Link>
-                </li>
-                <li> <Link href="/login/loginstudent">Student Login</Link></li>
-
-                <li>  <Link href="/login/loginteacher">Teacher Login</Link></li>
-               
-              
-            
-        </ul>
         {
-            children
+            pathName!=="/login/loginteacher"?<ul className="list-none flex justify-center items-center gap-10 p-4 bg-slate-700 text-white">
+            <li>
+              <Link href="/login">Login Main</Link>
+            </li>
+            <li>
+              {" "}
+              <Link href="/login/loginstudent">Student Login</Link>
+            </li>
+    
+            <li>
+              {" "}
+              <Link href="/login/loginteacher">Teacher Login</Link>
+            </li>
+          </ul>:null
         }
+      
+      {children}
     </div>
-)
+  );
 }
